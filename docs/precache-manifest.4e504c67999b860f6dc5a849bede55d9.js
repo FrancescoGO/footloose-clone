9efe24d388063551fa95ac8b7dbd04a9ad804334
FrancescoGO <francesco.go.os@gmail.com>
2020-08-06 21:38:21 -0500

    Test 4

---
+++ docs/precache-manifest.4e504c67999b860f6dc5a849bede55d9.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a711e2dd8aefc3b95a5409901d5dda6",
+    "url": "/footloose-clone/index.html"
+  },
+  {
+    "revision": "1034254d62f9975c6ede",
+    "url": "/footloose-clone/static/css/main.e53a8068.chunk.css"
+  },
+  {
+    "revision": "c633013002903cc95a9d",
+    "url": "/footloose-clone/static/js/2.f4f5b5e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/footloose-clone/static/js/2.f4f5b5e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1034254d62f9975c6ede",
+    "url": "/footloose-clone/static/js/main.13aa65db.chunk.js"
+  },
+  {
+    "revision": "6a24191f364159894878",
+    "url": "/footloose-clone/static/js/runtime-main.c8e6a8d2.js"
+  },
+  {
+    "revision": "150b4d483fda148e661de7f6ce84f88c",
+    "url": "/footloose-clone/static/media/advertisements-1.150b4d48.gif"
+  },
+  {
+    "revision": "7aa8a4404875346da1f4e7e3ebc7333e",
+    "url": "/footloose-clone/static/media/advertisements-2.7aa8a440.gif"
+  },
+  {
+    "revision": "58509a79716f062f8e5c416eb0ac91b5",
+    "url": "/footloose-clone/static/media/banner-advertisements.58509a79.gif"
+  },
+  {
+    "revision": "b7375330c20f5a6da3ea45b1a1c41347",
+    "url": "/footloose-clone/static/media/banner_accesorios_30.b7375330.webp"
+  },
+  {
+    "revision": "d2e1c48603f0c6e0875117211bc08b56",
+    "url": "/footloose-clone/static/media/banner_canjea_vale30s.d2e1c486.webp"
+  },
+  {
+    "revision": "5eba336dcf1762a6fd8658eda3806e4b",
+    "url": "/footloose-clone/static/media/banner_diversion_50calzado_ninos.5eba336d.webp"
+  },
+  {
+    "revision": "c90c2318547bc67380a26a8a516ab1ff",
+    "url": "/footloose-clone/static/media/banner_invierno_40.c90c2318.webp"
+  },
+  {
+    "revision": "e9cddf65a3992f1f69c87aba31c26093",
+    "url": "/footloose-clone/static/media/banner_nuevos_ingresos.e9cddf65.webp"
+  },
+  {
+    "revision": "f772e4d091c3e75e69e2e3f9681b344b",
+    "url": "/footloose-clone/static/media/banner_zapatillas_50.f772e4d0.gif"
+  },
+  {
+    "revision": "9c6d07763120a43ed032255e8b83e634",
+    "url": "/footloose-clone/static/media/company_logo.9c6d0776.jpg"
+  },
+  {
+    "revision": "dd46f71997e089e2b716b104e719fe62",
+    "url": "/footloose-clone/static/media/ma-adidas.dd46f719.webp"
+  },
+  {
+    "revision": "2fad8651c242b86ae087a4aa27f28870",
+    "url": "/footloose-clone/static/media/ma-beirario.2fad8651.webp"
+  },
+  {
+    "revision": "c459b5de429dee393d7cc1430de3e674",
+    "url": "/footloose-clone/static/media/ma-calimod.c459b5de.webp"
+  },
+  {
+    "revision": "1028872c705082abc3c8c9eae1786878",
+    "url": "/footloose-clone/static/media/ma-converse.1028872c.webp"
+  },
+  {
+    "revision": "1054a4688fcebb79d7246c11b0ee524b",
+    "url": "/footloose-clone/static/media/ma-dauss.1054a468.webp"
+  },
+  {
+    "revision": "e1e900b93d2fa36feafdd88e6beb2666",
+    "url": "/footloose-clone/static/media/ma-faena.e1e900b9.webp"
+  },
+  {
+    "revision": "fdd504a31245aa52cc1ff3fce8cc1d49",
+    "url": "/footloose-clone/static/media/ma-footloose.fdd504a3.webp"
+  },
+  {
+    "revision": "d9b8cc54dcc8b19004d378f5cc8cf75b",
+    "url": "/footloose-clone/static/media/ma-fresh.d9b8cc54.webp"
+  },
+  {
+    "revision": "5b044c2e90ff5c7c5ee25175af61860b",
+    "url": "/footloose-clone/static/media/ma-grendha.5b044c2e.webp"
+  },
+  {
+    "revision": "3d5991298cf5ed1794e9296cca34c3f7",
+    "url": "/footloose-clone/static/media/ma-hitec.3d599129.webp"
+  },
+  {
+    "revision": "db8a11122c992528add9b639e0fc17c6",
+    "url": "/footloose-clone/static/media/ma-ipanema.db8a1112.webp"
+  },
+  {
+    "revision": "143802a48b6aaa9701adeb049b154c56",
+    "url": "/footloose-clone/static/media/ma-modare.143802a4.webp"
+  },
+  {
+    "revision": "0918486a56bb7e98a4a2175a832e4938",
+    "url": "/footloose-clone/static/media/ma-moleca.0918486a.webp"
+  },
+  {
+    "revision": "6dbed49f4374de495101e67a55fa33a0",
+    "url": "/footloose-clone/static/media/ma-nike.6dbed49f.webp"
+  },
+  {
+    "revision": "8e68c7facc258f4b900302b36fd336d9",
+    "url": "/footloose-clone/static/media/ma-puma.8e68c7fa.webp"
+  },
+  {
+    "revision": "c627c2d776c2ba2d70629a8372957896",
+    "url": "/footloose-clone/static/media/ma-r18.c627c2d7.webp"
+  },
+  {
+    "revision": "353ef456d18a27ba542ba50892381e6d",
+    "url": "/footloose-clone/static/media/ma-reebok.353ef456.webp"
+  },
+  {
+    "revision": "988251d687dd5b8d2a564cf6fe3f8ea5",
+    "url": "/footloose-clone/static/media/ma-renzorenzini.988251d6.webp"
+  },
+  {
+    "revision": "d8f7404e1a7e875280b23fa0eea64ad6",
+    "url": "/footloose-clone/static/media/ma-rider.d8f7404e.webp"
+  },
+  {
+    "revision": "d248512cd621966d5cb7605d4200331f",
+    "url": "/footloose-clone/static/media/ma-tmsport.d248512c.webp"
+  },
+  {
+    "revision": "a6b6d1756f0f925f63a323c7efa081d0",
+    "url": "/footloose-clone/static/media/ma-topmodel.a6b6d175.webp"
+  },
+  {
+    "revision": "c206b6e18b9ee05c2e3e91d5f9134eda",
+    "url": "/footloose-clone/static/media/ma-umbro.c206b6e1.webp"
+  },
+  {
+    "revision": "6885dde1824ea68de7f966b2c54332d1",
+    "url": "/footloose-clone/static/media/ma-vans.6885dde1.webp"
+  },
+  {
+    "revision": "22fe718f2e2eca09155c68c8a4253e1f",
+    "url": "/footloose-clone/static/media/ma-vizzano.22fe718f.webp"
+  }
+]);
